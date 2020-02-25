39cd89ccdcba38411d9c6767d2a117c1181f85af
Filip Parag <filiparag.com@gmail.com>
2020-02-25 13:05:43 +0100

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Button class="ui button">Learn React</Button>
+      </header>
+      <body>
+        
+      </body>
+    </div>
+  );
+}
+
+export default App;
